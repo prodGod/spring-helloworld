a892860f4e9ff471c8abd4ee0d98cf592321b01a
prod.god <prod.god666@gmail.com>
2021-09-19 21:01:09 +0300

    Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean0 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean1 =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.equals(bean0));
+        System.out.println(bean2.equals(bean1));
+
     }
 }
